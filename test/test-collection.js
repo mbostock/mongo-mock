bee4fe45f0d7cdd54a1d9e00ea9d270b214e1b2f
Mike Bostock <mbostock@gmail.com>
2011-07-16 20:22:03 -0700

    Condense test specification.

--- test/test-collection.js
+++ test/test-collection.js
@@ -12,33 +12,20 @@ db.open(function(error) {
 });
 console.log("");
 
-console.log("empty + find:");
 db.open(function(error) {
   db.collection("empty", function(error, collection) {
-    collection.find({}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
+    collection.find({}, log("empty + find"));
   });
 });
 
-console.log("insert(object):");
 db.open(function(error) {
   db.collection("insert", function(error, collection) {
     collection.insert({hello: "world"}, function(error) {
-      collection.find({}, function(error, cursor) {
-        cursor.each(function(error, object) {
-          if (object) console.log("  " + JSON.stringify(object));
-          else console.log("");
-        });
-      });
+      collection.find({}, log("insert(object)"));
     });
   });
 });
 
-console.log("insert(objects):");
 db.open(function(error) {
   db.collection("inserts", function(error, collection) {
     collection.insert([
@@ -50,276 +37,65 @@ db.open(function(error) {
       {object: {"boolean": true, "number": 42, "array": [1, 2, "three"]}},
       {array: [1, 2, "three"]}
     ], function(error) {
-      collection.find({}, function(error, cursor) {
-        cursor.each(function(error, object) {
-          if (object) console.log("  " + JSON.stringify(object));
-          else console.log("");
-        });
-      });
+      collection.find({}, log("insert(objects)"));
     });
   });
 });
 
 db.open(function(error) {
   db.collection("inserts", function(error, collection) {
-    console.log("find(string = \"string\"):");
-    collection.find({string: "string"}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number = 42):");
-    collection.find({number: 42}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number >= 42):");
-    collection.find({number: {$gte: 42}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number > 41):");
-    collection.find({number: {$gt: 41}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number <= 42):");
-    collection.find({number: {$lte: 42}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number < 43):");
-    collection.find({number: {$lt: 43}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number < 43 and number > 41):");
-    collection.find({number: {$gt: 41, $lt: 43}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number != 42):");
-    collection.find({number: {$ne: 42}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number < 42):");
-    collection.find({number: {$lt: 42}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number <= 41):");
-    collection.find({number: {$lte: 41}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number > 42):");
-    collection.find({number: {$gt: 42}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number >= 43):");
-    collection.find({number: {$gte: 43}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(number >= 43 and number <= 41):");
-    collection.find({number: {$lte: 41, $gte: 43}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(boolean = true):");
-    collection.find({boolean: true}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(boolean = false):");
-    collection.find({boolean: false}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(boolean != true):");
-    collection.find({boolean: {$ne: true}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(null = null):");
-    collection.find({null: null}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(string > strinf):");
-    collection.find({string: {$gt: "strinf"}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(string < strinh):");
-    collection.find({string: {$lt: "strinh"}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(/^STR/i.test(string)):");
-    collection.find({string: {$regex: "^STR", $options: "i"}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(object.boolean = true and object.number > 41):");
-    collection.find({"object.boolean": true, "object.number": {$gt: -1}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date = 2011-01-01):");
-    collection.find({date: new Date(2011, 0, 1)}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date >= 2011-01-01):");
-    collection.find({date: {$gte: new Date(2011, 0, 1)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date > 2010-12-31):");
-    collection.find({date: {$gt: new Date(2010, 11, 31)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date <= 2011-01-01)):");
-    collection.find({date: {$lte: new Date(2011, 0, 1)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date < 2011-01-02):");
-    collection.find({date: {$lt: new Date(2011, 0, 2)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date < 2011-01-02 and date > 2010-12-31):");
-    collection.find({date: {$gt: new Date(2010, 11, 31), $lt: new Date(2011, 0, 2)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date != 2011-01-01):");
-    collection.find({date: {$ne: new Date(2011, 0, 1)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date < 2011-01-01):");
-    collection.find({date: {$lt: new Date(2011, 0, 1)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date <= 2010-12-31):");
-    collection.find({date: {$lte: new Date(2010, 11, 31)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date > 2011-01-01):");
-    collection.find({date: {$gt: new Date(2011, 0, 1)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date >= 2011-01-02):");
-    collection.find({date: {$gte: new Date(2011, 0, 2)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(date >= 2011-01-02 and date <= 2010-12-31):");
-    collection.find({date: {$lte: new Date(2010, 11, 31), $gte: new Date(2011, 0, 2)}}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(_id = ObjectId(4e21d7d80123ab0123000004)):");
-    collection.find({_id: new ObjectId("4e21d7d80123ab0123000004")}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
-    });
-    console.log("find(array = [1,2,\"three\"]):");
-    collection.find({array: [1, 2, "three"]}, function(error, cursor) {
-      cursor.each(function(error, object) {
-        if (object) console.log("  " + JSON.stringify(object));
-        else console.log("");
-      });
+    collection.find({string: "string"}, log("find(string = \"string\")"));
+    collection.find({number: 42}, log("find(number = 42)"));
+    collection.find({number: {$gte: 42}}, log("find(number >= 42)"));
+    collection.find({number: {$gt: 41}}, log("find(number > 41)"));
+    collection.find({number: {$lte: 42}}, log("find(number <= 42)"));
+    collection.find({number: {$lt: 43}}, log("find(number < 43)"));
+    collection.find({number: {$gt: 41, $lt: 43}}, log("find(number < 43 and number > 41)"));
+    collection.find({number: {$ne: 42}}, log("find(number != 42)"));
+    collection.find({number: {$lt: 42}}, log("find(number < 42)"));
+    collection.find({number: {$lte: 41}}, log("find(number <= 41)"));
+    collection.find({number: {$gt: 42}}, log("find(number > 42)"));
+    collection.find({number: {$gte: 43}}, log("find(number >= 43)"));
+    collection.find({number: {$lte: 41, $gte: 43}}, log("find(number >= 43 and number <= 41)"));
+    collection.find({boolean: true}, log("find(boolean = true)"));
+    collection.find({boolean: false}, log("find(boolean = false)"));
+    collection.find({boolean: {$ne: true}}, log("find(boolean != true)"));
+    collection.find({null: null}, log("find(null = null)"));
+    collection.find({string: {$gt: "strinf"}}, log("find(string > strinf)"));
+    collection.find({string: {$lt: "strinh"}}, log("find(string < strinh)"));
+    collection.find({string: {$regex: "^STR", $options: "i"}}, log("find(/^STR/i.test(string))"));
+    collection.find({"object.boolean": true, "object.number": {$gt: -1}}, log("find(object.boolean = true and object.number > 41)"));
+    collection.find({date: new Date(2011, 0, 1)}, log("find(date = 2011-01-01)"));
+    collection.find({date: {$gte: new Date(2011, 0, 1)}}, log("find(date >= 2011-01-01)"));
+    collection.find({date: {$gt: new Date(2010, 11, 31)}}, log("find(date > 2010-12-31)"));
+    collection.find({date: {$lte: new Date(2011, 0, 1)}}, log("find(date <= 2011-01-01))"));
+    collection.find({date: {$lt: new Date(2011, 0, 2)}}, log("find(date < 2011-01-02)"));
+    collection.find({date: {$gt: new Date(2010, 11, 31), $lt: new Date(2011, 0, 2)}}, log("find(date < 2011-01-02 and date > 2010-12-31)"));
+    collection.find({date: {$ne: new Date(2011, 0, 1)}}, log("find(date != 2011-01-01)"));
+    collection.find({date: {$lt: new Date(2011, 0, 1)}}, log("find(date < 2011-01-01)"));
+    collection.find({date: {$lte: new Date(2010, 11, 31)}}, log("find(date <= 2010-12-31)"));
+    collection.find({date: {$gt: new Date(2011, 0, 1)}}, log("find(date > 2011-01-01)"));
+    collection.find({date: {$gte: new Date(2011, 0, 2)}}, log("find(date >= 2011-01-02)"));
+    collection.find({date: {$lte: new Date(2010, 11, 31), $gte: new Date(2011, 0, 2)}}, log("find(date >= 2011-01-02 and date <= 2010-12-31)"));
+    collection.find({_id: new ObjectId("4e21d7d80123ab0123000004")}, log("find(_id = ObjectId(4e21d7d80123ab0123000004))"));
+    collection.find({array: [1, 2, "three"]}, log("find(array = [1,2,\"three\"])"));
+  });
+});
+
+db.open(function(error) {
+  db.collection("save", function(error, collection) {
+    collection.save({hello: "world"}, function(error) {
+      collection.find({}, log("save(object) + find"));
     });
   });
 });
 
-console.log("save(object) + find:");
-db.open(function(error) {
-  db.collection("save", function(error, collection) {
-    collection.save({hello: "world"}, function(error) {
-      collection.find({}, function(error, cursor) {
-        cursor.each(function(error, object) {
-          if (object) console.log("  " + JSON.stringify(object));
-          else console.log("");
-        });
-      });
+function log(message) {
+  return function(error, cursor) {
+    console.log(message + ":");
+    cursor.each(function(error, object) {
+      if (object) console.log("  " + JSON.stringify(object));
+      else console.log("");
     });
-  });
-});
+  };
+}
